152d7b646d56e68de85d80f2ef4f30f145d8f2ef
Ryan Huynh <rhuynh7@hotmail.com>
2024-03-04 04:55:38 -0700

    Generate a Random Short URL ID

--- express_server.js
+++ express_server.js
@@ -1,9 +1,28 @@
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let randomString = '';
+  const stringLength = 6;
+
+  for (let i = 0; i < stringLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters[randomIndex];
+  }
+
+  return randomString;
+}
+
+// const randomString = generateRandomString();
+// console.log(randomString);
+
+
 app.set("view engine", "ejs")
 
+function generateRandomString() {}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -11,6 +30,11 @@ const urlDatabase = {
 };
 //This needs to come before all of our routes
 app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
